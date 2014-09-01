341506c91e44f1e95490ea97508d08eadaaf66b2
Carlos Lozano <carlos@adaptive.me>
2014-09-01 18:42:28 +0200

    Windows, Windows Phone & Silverlight 8.1 targets.

--- adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/Properties/AssemblyInfo.cs
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/Properties/AssemblyInfo.cs
@@ -1,18 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Adaptive Runtime Platform")]
-[assembly: AssemblyDescription("Adaptive Runtime Platform ARP Windows 8.1 Implementation")]
+[assembly: AssemblyDescription("Adaptive Runtime Platform ARP Windows Phone Silverlight 8.1 Implementation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Adaptive.me")]
-[assembly: AssemblyProduct("Adaptive.Arp.Impl.Windows")]
+[assembly: AssemblyProduct("Adaptive.Arp.Impl.WinPhoneSilverlight")]
 [assembly: AssemblyCopyright("Copyright ©  2014 Carlos Lozano Diez t/a Adaptive.me")]
 [assembly: AssemblyTrademark("Adaptive.me ARP Adaptive Runtime Platform")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ccad07b-5ad1-4a10-aa34-26a823e3e8ad")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,9 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.18.0")]
 [assembly: AssemblyFileVersion("1.0.18.0")]
-[assembly: ComVisible(false)]
---
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/nuget.cmd
@@ -0,0 +1,1 @@
+..\..\tools\win\NuGet.exe pack Adaptive.Arp.Impl.WinPhoneSilverlight.csproj -Prop Configuration=Release
