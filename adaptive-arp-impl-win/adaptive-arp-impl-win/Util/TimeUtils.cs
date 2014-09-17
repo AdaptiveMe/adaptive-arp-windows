bfcc18331a13cb811a4ece4f0e46d507926445e0
Carlos Lozano <carlos@adaptive.me>
2014-09-17 20:40:49 +0200

    Privide equivalent to System.currentTimeMillis()

---
+++ adaptive-arp-impl-win/adaptive-arp-impl-win/Util/TimeUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaptive.Arp.Impl.Util
+{
+    public class TimeUtils
+    {
+        private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        private TimeUtils()
+        {
+
+        }
+        public static long CurrentTimeMillis()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+    }
+}
