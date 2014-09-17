dc35b24eef4ac2eaf1c5e18c6713b89181dd6071
Carlos Lozano <carlos@adaptive.me>
2014-09-17 20:42:26 +0200

    IAccelerometer Implementation v1.0.42.

--- adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/AccelerometerImpl.cs
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/AccelerometerImpl.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Adaptive.Arp.Api;
 using Adaptive.Arp.Impl;
+using Adaptive.Arp.Impl.Util;
 using Microsoft.Devices.Sensors;
 
 namespace Adaptive.Arp.Impl.WinPhoneSilverlight
@@ -16,15 +18,47 @@ namespace Adaptive.Arp.Impl.WinPhoneSilverlight
         public AccelerometerImpl() : base()
         {
             this.accelerometer = new Accelerometer();
+            this.accelerometer.CurrentValueChanged += accelerometer_CurrentValueChanged;
             this.isTrackingStarted = false;
         }
 
         protected override void startTracking()
         {
+            SensorState state = this.accelerometer.State;
+            
             if (!this.isTrackingStarted)
             {
                 this.accelerometer.Start();
-                this.isTrackingStarted = true;
+
+                if (state == SensorState.Disabled || state == SensorState.NotSupported) {
+                    notifyError(IAccelerationListener.Error.Unavailable);
+                }
+                else if (state == SensorState.NoPermissions)
+                {
+                    notifyError(IAccelerationListener.Error.Unauthorized);
+                }
+                else
+                {
+                    this.isTrackingStarted = true;
+                }
+            }
+        }
+
+        void accelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
+        {
+            SensorState state = this.accelerometer.State;
+            Acceleration acceleration = new Acceleration(e.SensorReading.Acceleration.X, e.SensorReading.Acceleration.Y, e.SensorReading.Acceleration.Z, TimeUtils.CurrentTimeMillis());
+            if (state == SensorState.Ready)
+            {
+                notifyResult(acceleration);
+            }
+            else if (state == SensorState.Initializing)
+            {
+                notifyWarning(acceleration, IAccelerationListener.Warning.NeedsCalibration);
+            }
+            else if (state == SensorState.NoData)
+            {
+                notifyWarning(acceleration, IAccelerationListener.Warning.Stale);
             }
         }
 
