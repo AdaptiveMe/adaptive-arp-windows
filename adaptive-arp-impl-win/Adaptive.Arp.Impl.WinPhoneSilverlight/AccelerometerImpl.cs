bb09a1caba4f26715194a68200fc430fae2c1c49
Carlos Lozano <carlos@adaptive.me>
2014-09-17 20:03:01 +0200

    IAccelerometer Implementation v1.0.42

---
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhoneSilverlight/AccelerometerImpl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adaptive.Arp.Impl;
+using Microsoft.Devices.Sensors;
+
+namespace Adaptive.Arp.Impl.WinPhoneSilverlight
+{
+    public class AccelerometerImpl : AbstractAccelerometerImpl
+    {
+        private Accelerometer accelerometer;
+        private bool isTrackingStarted;
+
+        public AccelerometerImpl() : base()
+        {
+            this.accelerometer = new Accelerometer();
+            this.isTrackingStarted = false;
+        }
+
+        protected override void startTracking()
+        {
+            if (!this.isTrackingStarted)
+            {
+                this.accelerometer.Start();
+                this.isTrackingStarted = true;
+            }
+        }
+
+        protected override void stopTracking()
+        {
+            if (isTrackingStarted)
+            {
+                this.accelerometer.Stop();
+                this.isTrackingStarted = false;
+            }
+        }
+
+        protected override bool isTracking()
+        {
+            return this.isTrackingStarted;
+        }
+    }
+}
---
+++ adaptive-arp-impl-win/adaptive-arp-impl-win/AbstractAccelerometerImpl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adaptive.Arp.Api;
+
+namespace Adaptive.Arp.Impl
+{
+    public abstract class AbstractAccelerometerImpl : IAccelerometer
+    {
+        private List<IAccelerationListener> listenerList;
+
+        public AbstractAccelerometerImpl()
+        {
+            listenerList = new List<IAccelerationListener>();
+        }
+
+        public void AddAccelerationListener(IAccelerationListener listener)
+        {
+            if (listener != null && !listenerList.Contains(listener))
+            {
+                listenerList.Add(listener);
+                if (!isTracking()) startTracking();
+            }
+        }
+
+        public void RemoveAccelerationListener(IAccelerationListener listener)
+        {
+            if (listener != null && listenerList.Contains(listener))
+            {
+                listenerList.Remove(listener);
+            }
+        }
+
+        public void RemoveAccelerationListeners()
+        {
+            listenerList.Clear();
+            if (isTracking()) stopTracking();
+        }
+
+        protected abstract void startTracking();
+
+        protected abstract void stopTracking();
+
+        protected abstract bool isTracking();
+
+        public void notifyResult(Acceleration acceleration)
+        {
+            foreach (IAccelerationListener listener in listenerList)
+            {
+                listener.OnResult(acceleration);
+            }
+        }
+
+        public void notifyWarning(Acceleration acceleration, IAccelerationListener.Warning warning)
+        {
+            foreach (IAccelerationListener listener in listenerList)
+            {
+                listener.OnWarning(acceleration, warning);
+            }
+        }
+
+        public void notifyError(IAccelerationListener.Error error)
+        {
+            foreach (IAccelerationListener listener in listenerList)
+            {
+                listener.OnError(error);
+            }
+        }
+    }
+}
