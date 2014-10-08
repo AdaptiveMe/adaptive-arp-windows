9e205f5d74fc2741fb7159f18b7ae2c76f4fca93
Carlos Lozano <carlos@adaptive.me>
2014-10-08 11:13:19 +0200

    Project update - introducing JSON.net.

---
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhone/Appverse/ContactPhone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaptive.Arp.Impl.WinPhone.Appverse
+{
+    public class ContactPhone
+    {
+        public ContactPhone()
+        {
+        }
+
+        public NumberType Type { get; set; }
+
+        public string Number { get; set; }
+
+        public bool IsPrimary { get; set; }
+    }
+}
---
+++ adaptive-arp-impl-win/Adaptive.Arp.Impl.WinPhone/Appverse/HardwareInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaptive.Arp.Impl.WinPhone.Appverse
+{
+    public class HardwareInfo
+    {
+        private string name;
+        private string vendor;
+        private string version;
+        private string deviceUuid;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Vendor
+        {
+            get
+            {
+                return vendor;
+            }
+            set
+            {
+                vendor = value;
+            }
+        }
+
+        public string UUID
+        {
+            get
+            {
+                return deviceUuid;
+            }
+            set
+            {
+                deviceUuid = value;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return version;
+            }
+            set
+            {
+                version = value;
+            }
+        }
+    }
+}
