3befc135b2bc2685dfafa6bbd9452fd91ff9b103
Carlos Lozano <carlos@adaptive.me>
2015-02-18 21:02:11 +0100

    API v2.1.8 Specification

--- adaptive-arp-lib/adaptive-arp-lib/Sources/Adaptive.Arp.Api/BaseCallbackImpl.cs
+++ adaptive-arp-lib/adaptive-arp-lib/Sources/Adaptive.Arp.Api/BaseCallbackImpl.cs
@@ -27,7 +27,7 @@ Contributors:
 
 Release:
 
-    * @version v2.1.7
+    * @version v2.1.8
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
@@ -88,7 +88,7 @@ namespace Adaptive.Arp.Api
           */
           public String GetAPIVersion()
           {
-               return "v2.1.7";
+               return "v2.1.8";
           }
 
           /**
